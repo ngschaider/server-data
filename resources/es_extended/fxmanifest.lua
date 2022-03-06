8e5bfb4cf4679011bbce9be511549d67ebb7777b
Niklas Gschaider <niklas.gschaider@gschaider-systems.at>
2022-03-06 18:37:07 +0100

    remove html/* files and clean fxmanifest

--- resources/es_extended/fxmanifest.lua
+++ resources/es_extended/fxmanifest.lua
@@ -11,18 +11,6 @@ server_scripts {
 
 	"boot/sh_modules.lua",
 	"boot/sh_main.lua",
-	
-	--"server/callbacks.lua",
-	--'server/common.lua',
-	--'server/classes/player.lua',
-	--'server/functions.lua',
-	--'server/paycheck.lua',
-	--'server/main.lua',
-	--'server/commands.lua',
-
-	--'shared/modules/math.lua',
-	--'shared/modules/table.lua',
-	--'shared/functions.lua'
 }
 
 client_scripts {
@@ -30,21 +18,6 @@ client_scripts {
 
 	"boot/sh_modules.lua",
 	"boot/sh_main.lua",
-
-	--"client/callbacks.lua",
-	--'client/common.lua',
-	--"client/character_selection.lua",
-	--'client/functions.lua',
-	--'client/wrapper.lua',
-	--'client/main.lua',
-
-	--'client/modules/death.lua',
-	--'client/modules/scaleform.lua',
-	--'client/modules/streaming.lua',
-
-	--'shared/modules/math.lua',
-	--'shared/modules/table.lua',
-	--'shared/functions.lua'
 }
 
 ui_page {
@@ -53,23 +26,7 @@ ui_page {
 
 files {
 	"modules.json",
-
-	'imports.lua',
 	'locale.js',
-	--'html/ui.html',
-
-	--'html/css/app.css',
-
-	--'html/js/mustache.min.js',
-	--'html/js/wrapper.js',
-	--'html/js/app.js',
-
-	--'html/fonts/pdown.ttf',
-	--'html/fonts/bankgothic.ttf',
-
-	--'html/img/accounts/bank.png',
-	--'html/img/accounts/black_money.png',
-	--'html/img/accounts/money.png'
 }
 
 dependencies {
