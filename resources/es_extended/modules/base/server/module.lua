ffc4b7b993ff1e2de6dc1329a9a119f0cb5e94c0
Niklas Gschaider <niklas.gschaider@gschaider-systems.at>
2022-03-07 00:38:03 +0100

    moved ngx to a submodule

--- resources/es_extended/modules/base/server/module.lua
+++
@@ -1,2 +0,0 @@
-SetMapName('San Andreas');
-SetGameType('NGX');
--- resources/es_extended/modules/utils/server/module.lua
+++
@@ -1,9 +0,0 @@
-module.GetIdentifier = function(playerId)
-	for k,v in pairs(GetPlayerIdentifiers(playerId)) do
-		if string.match(v, 'license:') then
-			return string.gsub(v, 'license:', '');
-		end
-	end
-
-	return nil;
-end
